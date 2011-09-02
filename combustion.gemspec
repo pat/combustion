cf64c1176fdefa498270fd21d1fde2755c837a05
Pat Allan <pat@freelancing-gods.com>
2011-09-02 15:30:13 +1000

    Basic generator via CLI tool.

--- combustion.gemspec
+++ combustion.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |s|
   s.require_paths = ['lib']
 
   s.add_runtime_dependency 'rails', '>= 3.1.0.rc5'
+  s.add_runtime_dependency 'thor',  '>= 0.14.6'
 end
---
+++ lib/combustion/generator.rb
@@ -0,0 +1,28 @@
+require 'thor/group'
+
+class Combustion::Generator < Thor::Group
+  include Thor::Actions
+
+  def self.source_root
+    File.expand_path File.join(File.dirname(__FILE__), '..', '..')
+  end
+
+  def create_directories
+    empty_directory 'spec/internal'
+    empty_directory 'spec/internal/config'
+    empty_directory 'spec/internal/db'
+    empty_directory 'spec/internal/log'
+    empty_directory 'spec/internal/public'
+  end
+
+  def create_files
+    template 'templates/routes.rb',    'spec/internal/config/routes.rb'
+    template 'templates/database.yml', 'spec/internal/config/database.yml'
+    template 'templates/schema.rb',    'spec/internal/db/schema.rb'
+    template 'templates/config.ru',    'config.ru'
+    create_file                        'spec/internal/public/favicon.ico'
+    create_file                        'spec/internal/log/.gitignore' do
+      '*.log'
+    end
+  end
+end
