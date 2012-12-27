123e56c6cbf5fafe7be6a80407c02b7c1e64c277
Philip Arndt <parndt@gmail.com>
2012-12-28 12:14:34 +1300

    Place class definitions inside module Combustion.
    This allows libraries to selectively load parts of Combustion without
    having to load the entire thing. For example, if you wanted to use the
    Database class then you can just use:
    
    require 'combustion/database'
    
    Previously this would require:
    
    require 'combustion'
    require 'combustion/database'
    
    As you would receive the following error message:
    
    combustion/lib/combustion/database.rb:1:in `<top (required)>': uninitialized constant Combustion (NameError)

--- lib/combustion/application.rb
+++ lib/combustion/application.rb
@@ -1,34 +1,36 @@
 Rails.env = 'test'
 
-class Combustion::Application < Rails::Application
-  # Core Settings
-  config.cache_classes               = true
-  config.whiny_nils                  = true
-  config.consider_all_requests_local = true
-  config.secret_token                = Digest::SHA1.hexdigest Time.now.to_s
+module Combustion
+  class Application < Rails::Application
+    # Core Settings
+    config.cache_classes               = true
+    config.whiny_nils                  = true
+    config.consider_all_requests_local = true
+    config.secret_token                = Digest::SHA1.hexdigest Time.now.to_s
 
-  # ActiveSupport Settings
-  config.active_support.deprecation = :stderr
+    # ActiveSupport Settings
+    config.active_support.deprecation = :stderr
 
-  # Some settings we're not sure if we want, so let's not load them by default.
-  # Instead, wait for this method to be invoked (to get around load-order
-  # complications).
-  def self.configure_for_combustion
-    config.root = File.expand_path File.join(Dir.pwd, Combustion.path)
+    # Some settings we're not sure if we want, so let's not load them by default.
+    # Instead, wait for this method to be invoked (to get around load-order
+    # complications).
+    def self.configure_for_combustion
+      config.root = File.expand_path File.join(Dir.pwd, Combustion.path)
 
-    if defined?(ActionController) && defined?(ActionController::Railtie)
-      config.action_dispatch.show_exceptions            = false
-      config.action_controller.perform_caching          = false
-      config.action_controller.allow_forgery_protection = false
-    end
+      if defined?(ActionController) && defined?(ActionController::Railtie)
+        config.action_dispatch.show_exceptions            = false
+        config.action_controller.perform_caching          = false
+        config.action_controller.allow_forgery_protection = false
+      end
 
-    if defined?(ActionMailer) && defined?(ActionMailer::Railtie)
-      config.action_mailer.delivery_method     = :test
-      config.action_mailer.default_url_options = {:host => 'www.example.com'}
-    end
+      if defined?(ActionMailer) && defined?(ActionMailer::Railtie)
+        config.action_mailer.delivery_method     = :test
+        config.action_mailer.default_url_options = {:host => 'www.example.com'}
+      end
 
-    if defined?(Sprockets)
-      config.assets.enabled = true
+      if defined?(Sprockets)
+        config.assets.enabled = true
+      end
     end
   end
 end
