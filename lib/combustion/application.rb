aee27c83b0dbf83f69346adc754e7308b89e1e62
Pat Allan <pat@freelancing-gods.com>
2013-05-01 16:24:01 -0700

    Shift whitelist setting into configure_for_combustion method.

--- lib/combustion/application.rb
+++ lib/combustion/application.rb
@@ -14,15 +14,17 @@ module Combustion
     # ActiveSupport Settings
     config.active_support.deprecation = :stderr
 
-    # Turn on ActiveRecord attribute whitelisting
-    # This way the dummy app matches new rails apps re: this setting
-    config.active_record.whitelist_attributes = true if config.respond_to? :active_record
-
-    # Some settings we're not sure if we want, so let's not load them by default.
-    # Instead, wait for this method to be invoked (to get around load-order
-    # complications).
+    # Some settings we're not sure if we want, so let's not load them by
+    # default. Instead, wait for this method to be invoked (to get around
+    # load-order complications).
     def self.configure_for_combustion
       config.root = File.expand_path File.join(Dir.pwd, Combustion.path)
+
+      if defined? ActiveRecord::Railtie
+        # Turn on ActiveRecord attribute whitelisting
+        # This way the dummy app matches new rails apps re: this setting
+        config.active_record.whitelist_attributes = true
+      end
 
       if defined? ActionController::Railtie
         config.action_dispatch.show_exceptions            = false
