b2f2dbbb5b1b2f7d2738aacbc8002c2cab657614
Pat Allan <pat@freelancing-gods.com>
2020-04-20 22:42:02 +1000

    Add explicit ActiveJob support.

--- lib/combustion.rb
+++ lib/combustion.rb
@@ -22,6 +22,7 @@ module Combustion
     }
 
     hash[:sprockets]      = "sprockets/railtie"     if Rails.version.to_f >= 3.1
+    hash[:active_job]     = "active_job/railtie"    if Rails.version.to_f >= 4.2
     hash[:active_storage] = "active_storage/engine" if Rails.version.to_f >= 5.2
 
     hash
