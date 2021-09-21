6549b202141b740ce46ed57bdec83a4841c4cb9a
Pat Allan <pat@freelancing-gods.com>
2021-09-21 23:03:31 +1000

    Shift ActiveRecord version check after Railtie check.
    This should resolve #118.

--- lib/combustion/configurations/active_record.rb
+++ lib/combustion/configurations/active_record.rb
@@ -2,12 +2,12 @@
 
 class Combustion::Configurations::ActiveRecord
   def self.call(config)
+    return unless defined?(ActiveRecord::Railtie)
+    return unless ::ActiveRecord.constants.include?(:MassAssignmentSecurity)
+
     if ActiveRecord::VERSION::MAJOR >= 7
       config.active_record.legacy_connection_handling = false
     end
-
-    return unless defined?(ActiveRecord::Railtie)
-    return unless ::ActiveRecord.constants.include?(:MassAssignmentSecurity)
 
     # Turn on ActiveRecord attribute whitelisting
     # This way the dummy app matches new rails apps re: this setting
